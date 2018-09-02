8e2d9731dd6ffa39efff1875541849d2b3fb82fd
RyanBoyes <league@iMac-2.attlocal.net>
2018-09-02 11:27:14 -0700

    robots
    :)

--- src/arrays/_01_RobotRace.java
+++ src/arrays/_01_RobotRace.java
@@ -4,6 +4,7 @@ package arrays;
 
 import java.util.Random;
 
+import javax.swing.JOptionPane;
 import javax.swing.text.StyledEditorKit.ForegroundAction;
 
 import org.jointheleague.graphical.robot.Robot;
@@ -12,25 +13,51 @@ public class _01_RobotRace {
 
 	//1. make a main method
 public static void main(String[] args) {
+	boolean finished = false;
 	Robot[] Rs = new Robot[5];
-
+	Robot[] Rz = new Robot[4];
 	//2. create an array of 5 robots.
 
 	//3. use a for loop to initialize the robots.
-for(int i = 0; i<Rs.length;i++) {
-	Rs[i] = new Robot();
+for(int i = 0; i<Rz.length;i++) {
+	Rz[i] = new Robot();
 }
 		//4. make each robot start at the bottom of the screen, side by side, facing up
-for(int a = 0; a<Rs.length;a++) {
-	Rs[a].setX(a*100+200); Rs[a].setY(550);
+for(int a = 0; a<Rz.length;a++) {
+	Rz[a].setX(a*100+90); Rz[a].setY(800);
 
 }
 	//5. use another for loop to iterate through the array and make each robot move 
 	//   a random amount less than 50.
 Random randy = new Random();
+
+
+while(!finished) {
+	for(int q = 0;q<Rz.length;q++) {
+		Rz[q].setSpeed(15);
+		int rand = randy.nextInt(100);
+		Rz[q].move(rand);
+		if(Rz[q].getY()<=0) {
+			JOptionPane.showMessageDialog(null, "Robot number " + q + " is the winner.");
+		}
+	}
+}
+
+
+
+
+
+
+while(!finished) {
 for(int z = 0;z<Rs.length;z++) {
-	int ran = randy.nextInt(50);
+	Rs[z].setSpeed(15);
+	int ran = randy.nextInt(100);
 	Rs[z].move(ran);
+if(Rs[z].getY()<=0) {
+	JOptionPane.showMessageDialog(null, "Robot number " + z + " is the winner.");
+}
+}
+
 }
 
 	//6. use a while loop to repeat step 5 until a robot has reached the top of the screen.
